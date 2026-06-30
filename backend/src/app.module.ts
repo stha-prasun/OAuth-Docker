71ae86e3e32a23b29f44232aa4e21f274725db64
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-06-30 10:50:57 +0545

    User Setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CourseModule } from './course/course.module';
-import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [AuthModule, UserModule, CourseModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    CourseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: Number(config.get<string>('DB_PORT')),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/user/entities/userEntity.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Role } from '../user.types';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column({ nullable: true })
+  password!: string;
+
+  @Column({ default: Role.Student })
+  role!: string;
+
+  @Column({ nullable: true, unique: true })
+  googleId!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
