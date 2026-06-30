a5ce51a9a3ac8d982a672d77972bc57990f7ec84
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-06-30 10:15:43 +0545

    Setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { CourseModule } from './course/course.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, CourseModule],
   controllers: [AppController],
   providers: [AppService],
 })
